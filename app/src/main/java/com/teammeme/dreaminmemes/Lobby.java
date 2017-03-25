6a8a0f289cd7d059bb8f6f7371b052314cb25ce5
Alexander Zhu <alexzhu@umich.edu>
2017-03-25 06:13:04 -0400

    Lobby save/load

--- app/src/main/java/com/teammeme/dreaminmemes/Lobby.java
+++ app/src/main/java/com/teammeme/dreaminmemes/Lobby.java
@@ -2,52 +2,91 @@ package com.teammeme.dreaminmemes;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.parse.GetCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
-/**
- * Created by Alex on 3/25/2017.
- */
+// TODO: Game_init
+// TODO: Game
 
 public class Lobby extends AppCompatActivity {
+    String name;
     ArrayList<Integer> players;  // Player IDs
-    LinkedList<Integer> judge_queue;  // Maintains order of judges
-    int round_num;
+    LinkedList<Integer> judgeQueue;  // Maintains order of judges
+    int roundNum;
+    boolean isJudge;
     State state;
 
     private enum State {
-        GameInit, ChoosePicture, Captioning, ChooseWinner
+        GameInit, ChoosePicture, Captioning, ChooseWinner, ShowScores
     }
 
-    static void store_blank_lobby(Integer owner_id) {
-        ArrayList<Integer> players = new ArrayList<Integer>();
-        LinkedList<Integer> judge_queue = new LinkedList<Integer>();
-        players.add(owner_id);
-        judge_queue.add(owner_id);
-        store_lobby(players, judge_queue, 1, State.GameInit);
+    // Store blank lobby (containing owner only) to DB
+    static String store_blank_lobby(Integer ownerId) {
+        ArrayList<Integer> players = new ArrayList<>();
+        LinkedList<Integer> judgeQueue = new LinkedList<>();
+        players.add(ownerId);
+        judgeQueue.add(ownerId);
+        return store_lobby("blank", players, judgeQueue, 1, State.GameInit);
     }
 
-    static void store_lobby(ArrayList<Integer> players, LinkedList<Integer> judge_queue,
-                            int round_num, State state) {
+    // Store lobby to DB
+    private static String store_lobby(String name, ArrayList<Integer> players,
+                            LinkedList<Integer> judgeQueue, int roundNum, State state) {
         // Write to DB
+        ParseObject dataObject = ParseObject.create("Lobby");
+        dataObject.put("name", name);
+        dataObject.put("players", players);
+        dataObject.put("judgeQueue", judgeQueue);
+        dataObject.put("roundNum", roundNum);
+        dataObject.put("state", state.ordinal());
+        dataObject.saveInBackground();
+        return dataObject.getObjectId();
+    }
+
+    // Load specified lobby from DB
+    void load_lobby(String lobbyId) {
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Lobby");
+        query.getInBackground(lobbyId, new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject object, ParseException e) {
+                if (e == null) {
+                    name = object.getString("name");
+                    List<Integer> temp = object.getList("players");
+                    players = new ArrayList<Integer>(temp);
+                    temp = object.getList("judgeQueue");
+                    judgeQueue = new LinkedList<Integer>(temp);
+                    roundNum = object.getInt("roundNum");
+                    state = State.values()[object.getInt("state")];
+                } else {
+                    Log.d("*****Lobby", "Error: " + e.getMessage());
+                }
+            }
+        });
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         // Select which layout to open
-        // get game id from init.getextra
-        // query DB with id
+        String lobbyId = getIntent().getStringExtra("ID");
+        load_lobby(lobbyId);
+        //boolean isJudge = false;
         load_layout();
-
+        //run();
     }
 
-    // Need to load different depending on judge or not...
+    // Load layout based on current state
     private void load_layout() {
         setContentView(R.layout.game_init);  // Temporary
-        /*Boolean is_judge = false;
-        if (is_judge) {
+        /*if (isJudge) {
             switch (state) {
                 case GameInit:
                     setContentView(R.layout.game_init_judge);
@@ -57,6 +96,8 @@ public class Lobby extends AppCompatActivity {
                     setContentView(R.layout.captioning_judge);
                 case ChooseWinner:
                     setContentView(R.layout.choose_winner_judge);
+                case ShowScores:
+                    setContentView(R.layout.show_scores);
             }
         } else {
             switch (state) {
@@ -68,8 +109,40 @@ public class Lobby extends AppCompatActivity {
                     setContentView(R.layout.captioning_others);
                 case ChooseWinner:
                     setContentView(R.layout.choose_winner_others);
+                case ShowScores:
+                    setContentView(R.layout.show_scores);
             }
         }*/
     }
 
+    // Perform different actions based on current state
+    private void run() {
+        if (isJudge) {
+            switch (state) {
+                case GameInit:
+                    // Input game information, invite people. Click start ->
+                case ChoosePicture:
+                    // Choose picture. Click post ->
+                case Captioning:
+                    // Idle. All players submit ->
+                case ChooseWinner:
+                    // Swipe. Select winner ->
+                case ShowScores:
+                    // Show scores. Next round starts in 1 minute ->
+            }
+        } else {
+            switch (state) {
+                case GameInit:
+                    // Wait for accept, idle if already accepted. Judge click start ->
+                case ChoosePicture:
+                    // Idle. Judge post ->
+                case Captioning:
+                    // Caption. Submit. All players submit ->
+                case ChooseWinner:
+                    // Idle. Judge select winner ->
+                case ShowScores:
+                    // Show scores. Next round starts in 1 minute ->
+            }
+        }
+    }
 }
